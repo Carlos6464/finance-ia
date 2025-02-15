c14263d089b33127edae93018770fd7194184335
Carlos6464 <adrianosodre23@gmail.com>
2025-02-15 10:22:35 -0300

    feat: ensure transactions and subscrptions pages are protected

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,6 +1,13 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
 
-const subscription = () => {
+const subscription = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
   return (
     <>
       <Navbar />
